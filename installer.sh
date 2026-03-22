85f2e86ca58b0ab4819466aef47d04b706be2293
ciefp <35612046+ciefp@users.noreply.github.com>
2026-03-22 09:31:53 +0100

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -98,7 +98,7 @@ sync
 echo ""
 echo ""
 echo "#########################################################"
-echo "#          CiefpBouquetUpdater INSTALLED SUCCESSFULLY          #"
+echo "#      CiefpBouquetUpdater INSTALLED SUCCESSFULLY       #"
 echo "#                  developed by ciefp                   #"
 echo "#                  .::CiefpSettings::.                  #"
 echo "#               https://github.com/ciefp                #"
